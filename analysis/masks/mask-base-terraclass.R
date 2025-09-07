a0f475faafd3825d10ec10247e3d7a0f651e13d5
Felipe Carlos <efelipecarlos@gmail.com>
2025-09-07 12:06:37 +0000

    mask: add terraclass preparation script

---
+++ analysis/masks/mask-base-terraclass.R
@@ -0,0 +1,18 @@
+library(sits)
+library(restoreutils)
+
+#
+# Config: Connection timeout
+#
+options(timeout = max(600, getOption("timeout")))
+
+
+#
+# 1) Download Terraclass data
+#
+restoreutils::prepare_terraclass(
+    years      = c(2008, 2010, 2012, 2014, 2018, 2020, 2022),
+    region_id  = 2,
+    multicores = 3
+)
+
