a222f49ad829ab5533b69388514c5fe730f5c5a2
Felipe Carlos <efelipecarlos@gmail.com>
2026-01-11 09:08:53 -0300

    masks: update mask rules

--- analysis/masks/mask-2021.R
+++ analysis/masks/mask-2021.R
@@ -79,7 +79,6 @@ eco_class <- load_restore_map_bdc(
   version    = classification_version
 )
 
-
 #
 # 4. Clean data to reduce noise
 #
@@ -224,13 +223,23 @@ eco_mask <- restoreutils::reclassify_rule21_pasture_annual_agriculture(
   version        = "step15"
 )
 
+eco_mask <- restoreutils::reclassify_rule28_secundary_vegetation_tc(
+  cube           = eco_mask,
+  mask           = NULL,
+  multicores     = multicores,
+  memsize        = memsize,
+  output_dir     = output_dir,
+  rarg_year      = classification_year,
+  version        = "step16"
+)
+
 eco_mask <- restoreutils::reclassify_rule2_current_deforestation(
   cube       = eco_mask,
   mask       = prodes,
   multicores = multicores,
   memsize    = memsize,
   output_dir = output_dir,
-  version    = "step16",
+  version    = "step17",
   rarg_year  = classification_year # <- rule argument: Deforestation year
 )
 
@@ -240,7 +249,7 @@ eco_mask <- restoreutils::reclassify_rule12_non_forest(
   multicores = multicores,
   memsize    = memsize,
   output_dir = output_dir,
-  version    = "step17"
+  version    = "step18"
 )
 
 eco_mask <- restoreutils::contextual_cleaner(
@@ -251,7 +260,7 @@ eco_mask <- restoreutils::contextual_cleaner(
   multicores   = multicores,
   memsize      = memsize,
   output_dir   = output_dir,
-  version      = "step18"
+  version      = "step19"
 )
 
 eco_mask <- restoreutils::reclassify_rule11_water(
@@ -260,7 +269,7 @@ eco_mask <- restoreutils::reclassify_rule11_water(
   multicores = multicores,
   memsize    = memsize,
   output_dir = output_dir,
-  version    = "step19"
+  version    = "step20"
 )
 
 eco_mask <- restoreutils::reclassify_rule19_perene(
@@ -270,7 +279,7 @@ eco_mask <- restoreutils::reclassify_rule19_perene(
   memsize    = memsize,
   output_dir = output_dir,
   rarg_year  = classification_year,
-  version    = "step20"
+  version    = "step21"
 )
 
 
@@ -281,19 +290,8 @@ eco_mask <- sits_mosaic(
   roi        = eco_region_roi,
   multicores = multicores,
   output_dir = output_dir,
-  version    = "step21"
+  version    = "step22"
 )
-
-# Generate stats
-cube_save_area_stats(
-  cube       = eco_mask,
-  multicores = multicores,
-  memsize    = memsize,
-  res        = 30,
-  output_dir = output_dir,
-  version    = "step21"
-)
-
 
 #
 # 6. Save cube object
