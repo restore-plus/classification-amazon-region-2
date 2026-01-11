302c99c0876255d5ec96b4c8407b7629e5829cd1
Felipe Carlos <efelipecarlos@gmail.com>
2026-01-11 09:09:17 -0300

    masks: add script to generate water mask

---
+++ analysis/masks/mask-base-water.R
@@ -0,0 +1,15 @@
+library(sits)
+library(restoreutils)
+
+#
+# General definitions
+#
+multicores <- 100
+
+#
+# 1) Prepare water mask
+#
+restoreutils::prepare_water_mask(
+  region_id        = 2,
+  multicores       = multicores
+)
